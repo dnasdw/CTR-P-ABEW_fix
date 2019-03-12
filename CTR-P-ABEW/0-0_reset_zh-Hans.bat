a3d73e6a42c9bd2b419e64009ebea3c6300ac463
Sun Daowen <sundaowen@gmail.com>
2019-03-12 09:47:10 +0800

    init CTR-P-ABEW

---
+++ CTR-P-ABEW/0-0_reset_zh-Hans.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "New Super Mario Bros. 2 (TWN) (zh-Hans).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hans_ABEWTOOLS.exe
+POPD
---
+++ CTR-P-ABEW/0-1_reset_zh-Hant.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "New Super Mario Bros. 2 (TWN) (zh-Hant).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hant_ABEWTOOLS.exe
+POPD
