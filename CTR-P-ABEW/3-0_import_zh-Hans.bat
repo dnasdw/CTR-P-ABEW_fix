3ff43cd8367d5585f8b745fd40a14c2d5c6f70a0
Sun Daowen <sundaowen@gmail.com>
2019-03-28 19:24:16 +0800

    update CTR-P-ABEW

--- CTR-P-ABEW/3-0_import_zh-Hans.bat
+++ CTR-P-ABEW/3-0_import_zh-Hans.bat
@@ -2,8 +2,8 @@ PUSHD "%~dp0"
 RD /S /Q build
 MD build\cxi0\exefs
 XCOPY cci\cxi0\exefs build\cxi0\exefs /S /Y
-MD build\cxi0\romfs
-XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
+REM MD build\cxi0\romfs
+REM XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
 tools\3dstool --lock -vf build\cxi0\exefs\code.bin --region CHN --language CN || PAUSE
 FOR /R build\cxi0\exefs\banner %%I IN (*.bcmdl) DO (tools\txobtool -ivfd "%%~I" "%%~dpnI" || PAUSE)
 POPD
